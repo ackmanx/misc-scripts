4acd17e6a900f19b38b9e228e21c0da4d58749a4
Eric Majerus <ackmanx@users.noreply.github.com>
2020-10-04 09:41:39 -0500

    update epoch-to-filename.js to work with wechat exports

--- scripts/photo/node/epoch-to-filename/epoch-to-filename.js
+++ scripts/photo/node/epoch-to-filename/epoch-to-filename.js
@@ -4,7 +4,7 @@
  * To run:
  * $ node epoch-to-filename.js
  *
- * You can run this from any directory and then just point to the script from there. The current directory will be the one it is executed from, not where the script resides. 
+ * You can run this from any directory and then just point to the script from there. The current directory will be the one it is executed from, not where the script resides.
  *
  * Risks:
  * Consider backup of files before running because they will be renamed with no undo
@@ -28,15 +28,23 @@ fs.readdir('.', function (err, files) {
 
             if (stat.isDirectory()) return;
 
-            var allDigitFilename = file.match(/^\d+\.jpg$/);
+            let formattedFilename = file
+
+            //If renaming WeChat images, we need to remove the prefix they add first
+            if (file.startsWith('mmexport')) {
+                formattedFilename = file.replace('mmexport', '')
+            }
+
+            var allDigitFilename = formattedFilename.match(/^\d+\.jpg$/);
             if (!allDigitFilename) {
                 console.warn('File must be in format of all digits: ', file);
                 return;
             }
 
-            const milliseconds = Number.parseInt(file.split('.')[0])
+            const milliseconds = Number.parseInt(formattedFilename.split('.')[0])
             const newDate  = moment(milliseconds).format(format)
 
+            //We pass the original filename to rename. The formattedFilename was only to build a new filename
             fs.renameSync(file, `${newDate}.jpg`);
         });
     });

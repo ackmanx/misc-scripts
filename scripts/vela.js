66f3d68fdcdc5a2161668454a7d43f25b8327d67
Eric Majerus <ackmanx@users.noreply.github.com>
2021-07-01 18:21:50 -0500

    Update vela.js

--- scripts/vela.js
+++ scripts/vela.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://vela.prod.target.com/GEM*
+// @match        *vela.prod.target.com/GEM*
 // @icon         https://www.google.com/s2/favicons?domain=target.com
 // @grant        none
 // ==/UserScript==
